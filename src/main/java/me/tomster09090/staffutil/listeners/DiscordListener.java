ac534e20998b6a5f40ca7f5d329ee35e36023fa4
tomster09090 <67745323+tomster09090@users.noreply.github.com>
2022-06-11 19:40:57 +0100

    Enabled the ability to turn on Discord to Minecraft chat, so that players in Minecraft do not see the Discord chat

--- src/main/java/me/tomster09090/staffutil/listeners/DiscordListener.java
+++ src/main/java/me/tomster09090/staffutil/listeners/DiscordListener.java
@@ -7,18 +7,9 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.plugin.Plugin;
-import sun.net.www.content.image.png;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 public class DiscordListener extends ListenerAdapter implements Listener {
 
@@ -30,10 +21,12 @@ public class DiscordListener extends ListenerAdapter implements Listener {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent e) {
-        if (e.getChannel().getName().equals(main.getConfig().getString("text-channel-name"))) {
-            if (!e.getMessage().getAuthor().isBot()) {
-                for (Player players : Bukkit.getOnlinePlayers()) {
-                    players.sendMessage(CC.RED + e.getMember().getUser().getAsTag() + CC.GRAY + ": " + e.getMessage().getContentRaw());
+        if (main.getConfig().getBoolean("discord-chat-to-minecraft")) {
+            if (e.getChannel().getName().equals(main.getConfig().getString("text-channel-name"))) {
+                if (!e.getMessage().getAuthor().isBot()) {
+                    for (Player players : Bukkit.getOnlinePlayers()) {
+                        players.sendMessage(CC.RED + e.getMember().getUser().getAsTag() + CC.GRAY + ": " + e.getMessage().getContentRaw());
+                    }
                 }
             }
         }
